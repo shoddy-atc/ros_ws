df862a43f899a18516a4b8a590b71f898b870497
shoddy_atc <tsumagoi0221@gmail.com>
2025-06-22 13:24:43 +0900

    ros2 bridge

--- src/ignition_lecture/launch/basic_world1.launch.py
+++ src/ignition_lecture/launch/basic_world1.launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
 from launch.substitutions import PathJoinSubstitution
 from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
@@ -13,11 +14,21 @@ def generate_launch_description():
         'basic_world1.sdf'
     ])
 
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
+            '/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan'
+        ]
+    )
+
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([os.path.join(
                 get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
             launch_arguments=[
                 ('gz_args', world_file_path)]
-        )
+        ),
+        bridge
     ])
